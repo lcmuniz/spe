a4d0eb66d8d0c3e17a21976f3751e5b89292d177
Luiz Carlos Melo Muniz <lcmuniz@tcema.tc.br>
2025-10-25 18:42:02 -0300

    deploy

--- quasar.config.js
+++ quasar.config.js
@@ -74,6 +74,7 @@ export default defineConfig((/* ctx */) => {
     devServer: {
       // https: true,
       open: true, // opens browser window automatically
+      allowedHosts: ['spe.eficaz.online'],
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
